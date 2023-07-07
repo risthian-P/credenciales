b376ed1b764743129cce20773369acbcea52e8c3
Cristhian Panora <cristhian.panora@gmail.com>
2023-07-07 12:16:31 -0500

    credenciales

---
+++ src/credencial.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class credencial {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton saveButton;
+
+
+}
