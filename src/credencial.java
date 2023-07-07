a0981998dec591cb0aa8f4d3a83601ed743e43a6
Cristhian Panora <cristhian.panora@gmail.com>
2023-07-07 12:25:08 -0500

    credenciales

--- src/credencial.java
+++ src/credencial.java
@@ -1,10 +1,19 @@
 import javax.swing.*;
 
 public class credencial {
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextField textField3;
-    private JButton saveButton;
+    private JTextField name;
+    private JTextField cedula;
+    private JTextField edad;
+    private JButton guardar;
+    private JPanel roolPanel;
+    private JButton cargar;
 
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("credencial");
+        frame.setContentPane(new credencial().roolPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 
 }
